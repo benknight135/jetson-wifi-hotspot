45c65dd6b60bb8593b884d23ae633dc955c3c225
Ben Knight <benknight135@gmail.com>
2021-10-12 12:32:35 +0100

    update startup script

--- jetson_wifi_hotspot_startup.py
+++ jetson_wifi_hotspot_startup.py
@@ -7,17 +7,17 @@ script_path = os.path.dirname(os.path.abspath(__file__))
 config_file = os.path.join(script_path, "hotspot.config")
 
 # read ssid and password from config file
-with open("file") as f:
-    ssid = f.readline()
-    pswd = f.readline()
+with open(config_file) as f:
+    ssid = f.readline().rstrip()
+    pswd = f.readline().rstrip()
 
 while True:
     time.sleep(1)
     result = subprocess.run(['lsusb'], stdout=subprocess.PIPE)
     print(result.stdout)
-    
+
     if ("Realtek RTL8188CUS" in str(result.stdout)):
         time.sleep(3)
         os.system("sudo create_ap wlan0 eth0 '"+ssid+"' '"+pswd+"' --freq-band 2.4 --no-virt -w 2")
-        
+
         break
